aaf323fd0b7f4fa9ba9865bb1e4206294ffa96c5
Abdur Razzaq Sakib <abdurrazzaksakib2025@gmail.com>
2026-09-22 12:00:56 +0600

    use effact add

--- src/app/books/page.tsx
+++ src/app/books/page.tsx
@@ -1,20 +1,25 @@
+"use client"
 import BookCard from "@/components/shared/$RPDYY3G/shared/BookCard";
 import { IBook } from "@/types/books.type";
+import { useEffect, useState } from "react";
 
-const getBooks = async () => {
-  try{
-    
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_SERVER_BASE_URL}/booksData.json`
-    );
-    const data = await res.json();
-    return data;
-  } catch (error) {
-    console.error("Error fetching books:", error)};
-};
 
-const Books = async () => {
-  const booksData = await getBooks();
+
+const Books = () => {
+  // const booksData = await getBooks();
+
+   const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    fetch("./booksData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching books:", error);
+      });
+  }, []);
 
   return (
     <section className="container mx-auto px-4 py-10 md:py-14">
@@ -34,7 +39,7 @@ const Books = async () => {
         </p>
       </div>
       <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
-        {booksData?.map((book: IBook) => (
+        {books?.map((book: IBook) => (
           <BookCard key={book.bookId} book={book} />
         ))}
       </div>
